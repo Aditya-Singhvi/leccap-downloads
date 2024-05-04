94413d43efbe9469a0e02fd1344a2d3c3eb7bbb8
Aditya-Singhvi <singhvi@umich.edu>
2024-05-04 00:35:33 -0700

    Add partial README

---
+++ README.md
@@ -0,0 +1,25 @@
+# University of Michigan Lecture Downloads
+
+The scripts included here enable University of Michigan-affiliated individuals to download lectures from the Lecture Capture system ([leccap.engin.umich.edu](https://leccap.engin.umich.edu)). 
+
+As ITS does not make any promises about how long course recordings are retained, downloading content is the best way to ensure that you can access it whenever you want. As of May 2024, course recordings seem to be preserved for at least three years. 
+
+Note that the [CAEN website](https://caen.engin.umich.edu/lecrecording/students/) states that:
+> Students who wish to obtain video file downloads of lecture recordings must contact their course instructors directly. Only the instructors of a course may authorize download access.
+
+As such, please contact the instructors of any courses you wish to download before using these scripts. 
+
+## Overview 
+The Python script in [`mass-download/`](/mass-download/) enables users to download **any subset** of lectures from **any number of courses**. This script should take no more than five minutes to set up and run (although the actual downloads may take far longer).
+
+Credit goes to [@ajaypillay](https://github.com/ajaypillay) for inspiring this project. A (slightly modified) version of his original script can be found in [`one-off-script/`](/one-off-script/). This script can be run in the browser console and enables users to download all the lectures for a single course.
+
+Note that a single 85-minute lecture video takes about 400 MB to store; please ensure you have enough storage availablle before using these scripts. 
+
+## Usage (Mass Download)
+
+## Usage (One-Off Script)
+
+
+
+
--- mass-download/yank_courses.py
+++ mass-download/yank_courses.py
@@ -1,3 +1,8 @@
+'''
+Author: Aditya Singhvi
+Date: May 04, 2024
+'''
+
 from datetime import date, datetime
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,7 +13,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium import webdriver
 import subprocess
 import os
-import sys
 import json
 
 
@@ -50,4 +54,203 @@ def findStringContainedInValue(
         match_strs = [match_str.lower() for match_str in match_strs]
 
     for match_str in match_strs:
-        if match_s
+        if match_str in value:
+            return match_str
+    return None
+
+
+def sanitize_filename(orig_filename: str) -> str:
+    """
+    Given a filename, sanitizes it to only allow POSIX-approved characters.
+
+    POSIX-approved characters are the English letters (upper and lower), digits,
+    the underscore (_), dash (-), and period (.).
+    """
+    if orig_filename is None:
+        return ""
+
+    def sanitize_char(c: chr) -> chr:
+        if c in FILENAME_ALLOWED_CHARS:
+            return c
+        elif c == " ":
+            return "_"
+        elif c == "/":
+            return "-"
+        else:
+            return "."
+
+    return "".join(sanitize_char(c) for c in orig_filename)
+
+
+def isAnyTimeClose(time: str, match_times: str, delta: int = 10) -> bool:
+    """
+    Given a time and a list of matchTimes as strs in the format '10:30 AM',
+    checks whether the first time is within delta minutes of any of the
+    matchTimes.
+    """
+    if time is None or match_times is None:
+        return False
+
+    t1 = datetime.strptime(time, "%H:%M %p")
+    for match in match_times:
+        try:
+            t2 = datetime.strptime(match, "%H:%M %p")
+            if abs(t2 - t1).seconds <= delta * 60:
+                return True
+        except ValueError:
+            print(f"\tERROR: Time {match} formatted improperly. Ignoring.")
+
+    return False
+
+# End Helper Functions ------------------------------------------------------
+
+
+# Primary Functions --------------------------------------------------------
+def startSession() -> webdriver.Chrome:
+    """
+    Opens a Selenium Chrome WebDriver at leccap.engin.umich.edu/leccap and 
+    prompts user to log in manually. Once login is complete, minimizes the
+    window and returns the webdriver. 
+    """
+    chrome_options = Options()
+    chrome_options.page_load_strategy = "eager"
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+    driver.get(f"{LECCAP_BASE_URL}/leccap")
+
+    try:
+        wait = WebDriverWait(driver, timeout=180)
+        wait.until(EC.title_contains("Lecture Recordings"))
+    except TimeoutException:
+        print("ERROR: Login timeout occured. Please restart script.")
+        driver.quit()
+        exit(0)
+
+    driver.minimize_window()
+    return driver
+
+
+def getCourseToURLMap(driver: webdriver.Chrome, year: int, course_names: list[str]) -> dict[str, str]:
+    """
+    Given a Selenium Chrome Webdriver
+    """
+    course_to_url = dict()
+    try:
+        driver.get(f"{LECCAP_BASE_URL}/leccap/{year}")
+        WebDriverWait(driver, timeout=30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "list-group"))
+        )
+        course_list = driver.execute_script(
+            "return document.getElementsByClassName('list-group')[0].children"
+        )
+        for course in course_list:
+            course_title = course.text
+            common_course_name = findStringContainedInValue(
+                course_title, course_names)
+            if (
+                common_course_name is not None
+                and common_course_name not in course_to_url
+            ):
+                print(
+                    f"\tFound course {common_course_name} under name {course_title}.")
+                course_to_url[common_course_name] = course.get_dom_attribute(
+                    "href")
+    except JavascriptException:
+        print(f"\tNo courses found for year {year}.")
+
+    return course_to_url
+
+
+def saveLinks(
+    driver: webdriver.Chrome,
+    course_url: str,
+    save_path: str,
+    title_filters: list[str] = [],
+    section_filters: list[str] = [],
+    time_filters: list[str] = [],
+):
+    course_url = f"{LECCAP_BASE_URL}{course_url}"
+    print(f"\tCourse URL: {course_url}")
+    print(f"\tSave Path: {save_path}")
+    os.makedirs(save_path, exist_ok=True)
+    driver.get(course_url)
+    try:
+        driver.set_script_timeout(60)
+        recordings = driver.execute_async_script(SCRIPT)
+
+        with open(f"{save_path}/links.txt", "w") as file:
+            for recording in recordings:
+                title = recording["title"]
+                section = recording["section"]
+                time = recording["date"][recording["date"].index("•") + 2:]
+
+                if (
+                    (
+                        len(title_filters) == 0
+                        and len(section_filters) == 0
+                        and len(time_filters) == 0
+                    )
+                    or findStringContainedInValue(title, title_filters) is not None
+                    or findStringContainedInValue(section, section_filters) is not None
+                    or isAnyTimeClose(time, time_filters)
+                ):
+                    title = sanitize_filename(title)
+                    section = sanitize_filename(section)
+                    time = sanitize_filename(time)
+                    url = recording["url"]
+                    print(f"{section}.{title}.{time}.mp4 {url}", file=file)
+
+        print(f"\tLinks saved for {save_path}")
+    except TimeoutException:
+        print(
+            f"\tScript timed out trying to get links for {course_url}. Ignoring.")
+
+
+def downloadVideos(save_path: str):
+    with open(f"{save_path}/download_output.txt", "w") as file:
+        process = subprocess.Popen(
+            "exec xargs < links.txt -P 0 -L 1 wget -O",
+            shell=True,
+            cwd=save_path,
+            stdout=file,
+            stderr=file,
+            start_new_session=True
+        )
+        print(
+            f"\tDownloading videos at {save_path}. To kill this process, simply run:\n\t\tkill -9 -{os.getpgid(process.pid)}"
+        )
+
+# End Primary Functions ----------------------------------------------------
+
+
+def main():
+    with open(f"{CONFIG_FILE}", 'r') as file:
+        config = json.loads(file.read())
+    start_year = config["start_year"]
+    output_path = config["directory_path"]
+    courses_to_yank = {k.lower(): v for k, v in config["courses"].items()}
+    print(courses_to_yank)
+
+    driver = startSession()
+
+    for year in range(start_year, date.today().year + 1):
+        print(f"\n\nWorking on {year}...")
+        course_to_url = getCourseToURLMap(driver, year, courses_to_yank.keys())
+        for course, url in course_to_url.items():
+            print(f"\nWorking on {course}...")
+            path = f'{output_path}/{course.replace(" ", "")}'
+            saveLinks(
+                driver,
+                url,
+                path,
+                courses_to_yank[course].get("title_filters", []),
+                courses_to_yank[course].get("section_filters", []),
+                courses_to_yank[course].get("time_filters", [])
+            )
+            downloadVideos(path)
+
+    print("Saved links to all course recordings!\n")
+
+
+if __name__ == "__main__":
+    main()
